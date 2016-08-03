0d44ea56d9462504d4a7e9ce1a3b4c89b67d95db
yourtone <yuetanlin@gmail.com>
2016-08-04 04:55:07 +0800

    mod multimodal_net

--- misc/netdef.lua
+++ misc/netdef.lua
@@ -39,5 +39,17 @@ function netdef.AxBB(nhA,nhB,NB,nhcommon,dropout)
     output=nn.Squeeze()(output)
     return nn.gModule({q,i},{output});
 end
+function netdef.QxII(nhA,nhB,NB,nhcommon,dropout)
+    dropout = dropout or 0
+    local q=nn.Identity()();
+    local i=nn.Identity()();
+    local qc=nn.Tanh()(nn.Linear(nhA,nhcommon)(nn.Dropout(dropout)(q))); -- nhA -> nhcommon
+    local ic=nn.Tanh()(nn.TemporalConvolution(nhB,nhcommon,1)(nn.Dropout(dropout)(i))); -- (nbatch*)NB*nhB -> (nbatch*)NB*nhcommon
+    qc=nn.Reshape(-1,nhcommon,false)(qc) -- for batch or non-batch
+    qc=nn.Replicate(NB,2)(qc);-- nhcommon -> NB*nhcommon
+    qc=nn.Squeeze(1)(qc)
+    local output=nn.CMulTable()({qc,ic});
+    return nn.gModule({q,i},{output});
+end
 
 return netdef;
